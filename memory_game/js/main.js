cfc0d0e72f9b867e8844eb60e40c7fc7aa2a12cf
pradeep kharb <pradeepkharb@pradeeps-iMac.lan1>
2018-11-01 19:08:27 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,14 +1,27 @@
+
+
 var cards = ["queen" , "queen" , "king" , "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
 
-if(cardsInPlay[0] === cardsInPlay[1]){
-    alert("You found a match!");
+var checkForMatch = function() {
+	if(cardsInPlay[0] === cardsInPlay[1]){
+         alert("You found a match!");
+     }
+     else{
+	     alert("Sorry, try again");
+     }
 }
-else{
-	alert("Sorry, try again");
+
+var flipCard = function(cardId) {
+
+	console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+    checkForMatch();
+
 }
+
+flipCard(1);
+
+
+
 
